2986bc1142788fb604e58694b55ddddde9e8427e
Заур Алескеров <azr1@bk.ru>
2023-10-06 20:35:42 +0300

    First workflow

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -120,7 +120,7 @@ class RecipeReadSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ('id', 'tags', 'author', 
+        fields = ('id', 'tags', 'author',
                   'ingredients', 'is_favorited',
                   'is_in_shopping_cart',
                   'name', 'image', 'text',
