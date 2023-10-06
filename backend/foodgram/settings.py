1943ce3f4a37b7411183e13901b166549d1dddda
Заур Алескеров <azr1@bk.ru>
2023-10-07 00:26:07 +0300

    First workflow

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env.str('SECRET_KEY', default='молодец')
 
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(',')
 
